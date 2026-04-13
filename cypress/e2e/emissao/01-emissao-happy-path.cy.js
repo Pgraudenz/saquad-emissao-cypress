46b7f25d8a88314b1b5c98cab3976e8f84e75363
diego graudenz <diegograudenz@gmail.com>
2026-04-13 08:26:39 -0300

    feat: estrutura inicial do projeto

---
+++ cypress/e2e/emissao/01-emissao-happy-path.cy.js
@@ -0,0 +1,14 @@
+descibre("Emissão -  Happy path",()=>{
+
+it ("deve acessar a página com sucesso",()=>{
+cy.visit("https://example.cypress.io");
+Cy.contains("type").click();
+
+
+});
+
+
+
+
+
+});
